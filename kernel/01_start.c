03c6fbfb581a831b1989526480b000af1f1ae789
Berat Emir Mutlu <beratem@eduroam-141-23-176-79.wlan.tu-berlin.de>
2025-10-25 18:49:12 +0200

    feat(kernel): A1 boot banner, call test_kernel, and endless routine per spec

---
+++ kernel/01_start.c
@@ -0,0 +1,23 @@
+#include <arch/bsp/uart.h>
+#include <kernel/kprintf.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <config.h>
+
+void start_kernel(){
+	
+	init_uart();
+
+	kprintf("=== Betriebssystem gestartet ===\n");
+	test_kernel();
+		
+	while(true){
+	    char c = uart_getc();
+		#pragma GCC diagnostic push
+		#pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
+		kprintf("Es wurde folgendes Zeichen eingegeben: %c, In Hexadezimal: %x, "
+		    "In Dezimal: %08i, Als Ptr: %p\n",
+			c, (unsigned int)c, (int)c, (void*)c);
+		#pragma GCC diagnostic pop
+	}
+}
