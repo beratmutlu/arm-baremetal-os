f1be9a145697878f5810a2af4067a100bb437a35
Berat Emir Mutlu <beratem@eduroam-141-23-173-248.wlan.tu-berlin.de>
2025-11-12 04:10:40 +0100

    linker script to ccode (2)

--- arch/cpu/stacks.c
+++ arch/cpu/stacks.c
@@ -1,16 +1,11 @@
-#include <arch/cpu/stacks.h>
+// arch/cpu/stacks.c
 #include <stddef.h>
 #include <stdint.h>
 
-__attribute__((aligned(8))) static unsigned char svc_stack[0x1000];
-__attribute__((aligned(8))) static unsigned char irq_stack[0x1000];
-__attribute__((aligned(8))) static unsigned char abt_stack[0x1000];
-__attribute__((aligned(8))) static unsigned char und_stack[0x1000];
-__attribute__((aligned(8))) static unsigned char sys_stack[0x1000];
+__attribute__((aligned(8))) unsigned char svc_stack[0x1000];
+__attribute__((aligned(8))) unsigned char irq_stack[0x1000];
+__attribute__((aligned(8))) unsigned char abt_stack[0x1000];
+__attribute__((aligned(8))) unsigned char und_stack[0x1000];
+__attribute__((aligned(8))) unsigned char sys_stack[0x1000];
 
 
-asm(".globl __stack_svc_end\n__stack_svc_end = svc_stack + 0x1000");
-asm(".globl __stack_irq_end\n__stack_irq_end = irq_stack + 0x1000");
-asm(".globl __stack_abt_end\n__stack_abt_end = abt_stack + 0x1000");
-asm(".globl __stack_und_end\n__stack_und_end = und_stack + 0x1000");
-asm(".globl __stack_sys_end\n__stack_sys_end = sys_stack + 0x1000");
--- include/arch/cpu/stacks.h
+++ include/arch/cpu/stacks.h
@@ -3,12 +3,6 @@
 
 #include <stdint.h>
 
-extern unsigned char __stack_svc_end[];
-extern unsigned char __stack_irq_end[];
-extern unsigned char __stack_abt_end[];
-extern unsigned char __stack_und_end[];
-extern unsigned char __stack_sys_end[];
-
 void arm_init_stacks(void);
 
 extern unsigned char vectors_table[];
