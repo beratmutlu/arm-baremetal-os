04090bbdbb154aa67fb68736a8f729ecaa238768
Berat Emir Mutlu <beratem@eduroam-141-23-175-94.wlan.tu-berlin.de>
2025-11-19 15:27:40 +0100

    refactor(kernel/panic): slim header, move heavy includes to c-file, add doxygen group

---
+++ kernel/panic.c
@@ -0,0 +1,18 @@
+#include <kernel/panic.h>
+#include <arch/bsp/uart.h>
+#include <kernel/kprintf.h>
+
+void _panic [[noreturn]] (const char *func_name, const char *msg) {
+    asm volatile("cpsid if");
+
+    uart_init();
+
+    kprintf("KERNEL PANIC in %s: %s\n", func_name, msg);
+    kprintf("System halted.\n");
+
+    while (true)
+    {
+        asm volatile("wfi");
+    }
+    
+}
