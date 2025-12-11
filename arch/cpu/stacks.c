dd0682a2979399f6d285a7cadc743b5bad94ede2
Berat Emir Mutlu <beratemutlu@gmail.com>
2025-12-11 08:16:49 +0100

    non vol stacks

--- arch/cpu/stacks.c
+++ arch/cpu/stacks.c
@@ -3,10 +3,10 @@
 #include <stdint.h>
 #include <kernel/exc_frame_layout.h>
 
-__attribute__((aligned(8))) volatile unsigned char svc_stack[EXC_STACK_SIZE];
-__attribute__((aligned(8))) volatile unsigned char irq_stack[EXC_STACK_SIZE];
-__attribute__((aligned(8))) volatile unsigned char abt_stack[EXC_STACK_SIZE];
-__attribute__((aligned(8))) volatile unsigned char und_stack[EXC_STACK_SIZE];
-__attribute__((aligned(8))) volatile unsigned char sys_stack[EXC_STACK_SIZE];
+__attribute__((aligned(8)))  unsigned char svc_stack[EXC_STACK_SIZE];
+__attribute__((aligned(8)))  unsigned char irq_stack[EXC_STACK_SIZE];
+__attribute__((aligned(8)))  unsigned char abt_stack[EXC_STACK_SIZE];
+__attribute__((aligned(8)))  unsigned char und_stack[EXC_STACK_SIZE];
+__attribute__((aligned(8)))  unsigned char sys_stack[EXC_STACK_SIZE];
 
 
